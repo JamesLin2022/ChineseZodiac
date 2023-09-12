eb477094badfe7889028878365589a92607d9a48
JamesLin2022 <james.09461@gmail.com>
2023-09-12 12:59:03 -0700

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ChineseZodiac.Models;
+
+namespace ChineseZodiac.Pages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public int Year { get; set; }
+
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        ViewData["Message"] = "Enter a year";
+        ViewData["Zodiac"] = "";
+        ViewData["Alert"] = "primary";
+        ViewData["Image"] = "";
+    }
+
+    public void OnPost()
+    {
+        if (Year < 1900 || Year > DateTime.Now.Year + 1) {
+            ViewData["Message"] = "Year must be between 1900 and next year";
+            ViewData["Zodiac"] = "";
+            ViewData["Alert"] = "danger";
+            ViewData["Image"] = "";
+        } else {
+            string zodiac = Utils.GetZodiac(Year);;
+            ViewData["Message"] = "Your zodiac is ";
+            ViewData["Zodiac"] = zodiac;
+            ViewData["Alert"] = "success";
+            ViewData["Image"] = "images\\" + zodiac.ToLower() + ".png";
+        }
+        
+    }
+}
